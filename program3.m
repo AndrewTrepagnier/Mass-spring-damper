21a6569043d5d4608344a11dacb486a71cd5d410
Andrew Trepagnier <160799843+AndrewTrepagnier@users.noreply.github.com>
2025-03-15 16:06:56 -0500

    problem 4 solution

---
+++ program3.m
@@ -0,0 +1,30 @@
+% There are many different ways to calculate the transient response of a
+% spring-mass-damper system. In linear systems, the frequency of a system's
+% response is exaclty equal to the frequency of the excitation. 
+
+% in this problem, the given force response is y(t) = 0.002cos(40t).
+% Therefore, w = 40 rad/s
+
+%In other words: The system's response equation is given y(t) = 0.002*cos(40*t) which describe the system's motion due to the external force.
+% But the external force equation is unknown f(t) = F_0*cos(w*t).
+
+%Since system response general form is y(t) = Y*cos(w*t) so Y = 0.002 and w = 40 are known
+%and since f(t) = F_0*cos(w*t), then F_o and w are unknown.
+
+%Define knowns of the system
+M = 150; %kg
+K = 60000; %N/m
+C = 1200; %N.s/m
+
+%Parameters of the system's response
+Y = 0.002;
+w = 40;
+
+%Calculations
+wn = sqrt(K/M);
+r = w/wn;
+Z = C/(2*sqrt(K*M));
+
+F_o = Y*K*(sqrt((1-r^2)^2 + (2*Z*r)^2));
+disp(F_o);
+disp(w);
